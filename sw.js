58d91b5c34f5615762b2c7526aa98efc85d8a6da
ToolBox Dev <toolbox@example.com>
2026-04-23 15:35:27 +0000

    Fix: upgrade service worker v2, compact view 130px min, large file warning, manifest update

--- sw.js
+++ sw.js
@@ -1,4 +1,14 @@
-const CACHE='freetoolhubs-v1';
-const ASSETS=['/','/css/style.css','/js/common.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res.ok&&e.request.method==='GET'){const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone))}return res})))});
+const CACHE='freetoolhubs-v2';
+const ASSETS=['/','/css/style.css','/js/common.js','/manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(r=>{
+    var fetchPromise=fetch(e.request).then(res=>{
+      if(res.ok){var clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone))}
+      return res;
+    }).catch(()=>r);
+    return r||fetchPromise;
+  }));
+});
